da8b3fee3cd5234ce0b162b42813253618f15a22
Jesse Costello-Good <jcg@google.com>
2019-06-13 09:37:23 -0700

    fix: address feedback from Alex (I825)

--- internal/e2e/rollup_code_splitting/outputgroups.spec.js
+++ internal/e2e/rollup_code_splitting/outputgroups.spec.js
@@ -13,22 +13,16 @@ function checkExists(name) {
 describe('outputgroups', () => {
   it('should produce a es2015 sourcemap', () => {
     checkExists('bundle.es2015.js');
-    checkExists('bundle_chunks_es2015/additional_entry.js');
-    checkExists('bundle_chunks_es2015/additional_entry.js.map');
     checkExists('bundle_chunks_es2015/main1.js');
     checkExists('bundle_chunks_es2015/main1.js.map');
   });
   it('should produce a es5_min sourcemap', () => {
     checkExists('bundle.min.js');
-    checkExists('bundle_chunks_min/additional_entry.js');
-    checkExists('bundle_chunks_min/additional_entry.js.map');
     checkExists('bundle_chunks_min/main1.js');
     checkExists('bundle_chunks_min/main1.js.map');
   });
   it('should produce a es5_min_debug sourcemap', () => {
     checkExists('bundle.min_debug.js');
-    checkExists('bundle_chunks_min_debug/additional_entry.js');
-    checkExists('bundle_chunks_min_debug/additional_entry.js.map');
     checkExists('bundle_chunks_min_debug/main1.js');
     checkExists('bundle_chunks_min_debug/main1.js.map');
   });
